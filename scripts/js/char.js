514a56ad2b1a1dad90c1b621c5ebf3b6049a0971
Jon Kalfayan <jonk1993@gmail.com>
2015-05-08 17:29:10 -0700

    added a secret ;)

---
+++ scripts/js/char.js
@@ -0,0 +1,40 @@
+
+
+
+window.alert("Happy Valentines Day Char <3")
+window.alert("Like the card said, you make my life sweeter and happier :)")
+window.alert("I love youuuu")
+window.alert("Don't you even think of preventing this site from creating more dialog boxes")
+window.alert("Especially considering how cute this is")
+window.alert("Who knows how long this thing can go on for?")
+window.alert("Let's find out")
+window.alert("Hope you don't have to be anywhere anytime soon")
+window.alert("Remember that time that we made a commerical together in San Diego?")
+window.alert("Totes were the hottest people on the beach ;)")
+window.alert("Also remember that time I was hella drunk and kept telling you I liked you?")
+window.alert("how embarrassing")
+window.alert("but true ;)")
+window.alert("Doo doo doo")
+window.alert("doo dee doo")
+window.alert("da dee daaaaa")
+window.alert("im a pretty good singer aren't i?")
+window.alert("i hope you are finding this funny")
+window.alert("its going to be really awkward if you don't find this funny")
+window.alert("time for a story")
+var r = confirm("Do you want to hear a story about dogs?")
+if (r == true) {
+    window.alert("Ok here goes! There once was a dog named Thorbert, he loved everyone and everything. One day, he was at a park in San Francisco Portrero Hill area when he saw the most beautiful blonde girl reading a book. Thorbert decided he definitely needed to jump on the aforementioned woman and profusely lick her face. So Thorbert did what he does best and trotted is way over, with his tail flopping back and worth and his tongue lazily dangled outside of his mouth, and licked that girl till her sides were sore from giggling. Thorbert and the girl became best friends and hung out all the time at the Portrero Hill park. The end!")
+    var z = window.confirm("Did you like the story?")
+    if (z == true) {
+        window.alert("Yay! Glad you liked it!")
+    } else {
+        window.alert("Damn, tough valentines day crowd")
+    }
+} else {
+    window.alert("Awww you didn't wanna hear the story? It was a really good one. Oh well.")
+}
+window.alert("also happy belated 8 month anniversary")
+window.alert("i had a great time with you in the city :)")
+window.alert("glad we could finally do valentines day together")
+window.alert("and i hope you were impressed by my sexy website creation skills")
+
